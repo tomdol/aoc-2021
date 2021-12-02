d4a51c95af64669d91988627f941df4f156ca2fe
tomdol <tomasz.dolbniak@protonmail.com>
2021-12-02 21:39:04 +0100

    Solution of part 2

--- day_01/solution.cpp
+++ day_01/solution.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <filesystem>
 #include <fstream>
 #include <iterator>
@@ -31,12 +32,35 @@ uint32_t counting_iterator_solution(const std::filesystem::path& input_file) {
     return counter.count();
 }
 
+uint32_t window_solution(const std::filesystem::path& input_file) {
+    std::ifstream depth_measurements(input_file);
+    auto first = std::istream_iterator<int32_t>(depth_measurements);
+    const auto last = std::istream_iterator<int32_t>();
+
+    uint32_t i = 0;
+    std::array<int32_t, 4> values{*first++, *first++, *first++, 0};
+    while(first != last) {
+        values[3] = *first++;
+        // 2 middle elements cancel each other out when subtracting 2 windows
+        // which means only edge elements from 2 windows have to be subtracted
+        if (values[3] - values[0] > 0)
+            ++i;
+        // shift the 3 last values left and make the last element a placeholder for the next iteration
+        std::rotate(std::begin(values), std::begin(values) + 1, std::end(values));
+    }
+
+    return i;
+}
+
 int main() {
     const std::string_view input_file_name = "input.txt";
     const std::filesystem::path file_path{input_file_name};
 
+    // part 1
     std::cout << "iterator solution: " << iterator_solution(file_path) << std::endl;
     std::cout << "counting iterator solution: " << counting_iterator_solution(file_path) << std::endl;
+    // part 2
+    std::cout << "sliding window solution: " << window_solution(file_path) << std::endl;
 
     return 0;
 }
